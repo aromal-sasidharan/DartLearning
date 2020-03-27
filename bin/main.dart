5f53f7c71f566dda1afeaf4126d8aedf2d2d206f
mahesh5 <mahesh.varadaraj01.com>
2020-03-27 23:27:25 +0800

    added function with named parameter

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,12 @@
 import 'package:DartLearning/DartLearning.dart' as DartLearning; // alias is DartLearning
 import 'package:DartLearning/functions/function.dart' as Functions; // alias is Functions
 import 'package:DartLearning/functions/FunctionWithParameters.dart'; 
+import 'package:DartLearning/functions/FunctionWithNamedParameters.dart';
 
 void main(List<String> arguments) {
   print('Hello world: ${DartLearning.calculate()}!');
   Functions.printMe();
   functionWithParameters("aromal"); // calling a function without aliases
+  functionWithNamedParameter(name: "mahesh");
 }
+``
