9d8d27eec05fbf98dd55f9e10c3eb813bb1c1868
mahesh5 <mahesh.varadaraj01.com>
2020-03-28 00:21:37 +0800

    functions with optinal parameters

--- bin/main.dart
+++ bin/main.dart
@@ -2,10 +2,13 @@ import 'package:DartLearning/DartLearning.dart' as DartLearning; // alias is Dar
 import 'package:DartLearning/functions/function.dart' as Functions; // alias is Functions
 import 'package:DartLearning/functions/FunctionWithParameters.dart'; 
 import 'package:DartLearning/functions/FunctionWithNamedParameters.dart';
+import 'package:DartLearning/functions/FunctionWithOptionalParamters.dart';
 
 void main(List<String> arguments) {
   print('Hello world: ${DartLearning.calculate()}!');
   Functions.printMe();
   functionWithParameters("aromal"); // calling a function without aliases
-  functionWithNamedParameter(name: "mahesh");
+  functionWithNamedParameter(name: "mahesh"); // calling a function with name parameters
+  funtionsWithOptionalParameter(name: "mahesh", age: 30); // calling a function with optional paramters
+  
 }
---
+++ lib/functions/FunctionWithOptionalParamters.dart
@@ -0,0 +1,3 @@
+void funtionsWithOptionalParameter({String name = "Awesome Dart", int age = 20}) {
+  print("Your name is $name and age is $age");
+}
