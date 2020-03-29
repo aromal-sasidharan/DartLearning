744fb5c812bafa3dfdd36f7cb83ec359112b417b
mahesh5 <mahesh.varadaraj01.com>
2020-03-29 20:18:39 +0800

    json serilization using dio

--- bin/main.dart
+++ bin/main.dart
@@ -2,10 +2,17 @@ import 'package:DartLearning/DartLearning.dart' as DartLearning; // alias is Dar
 import 'package:DartLearning/functions/function.dart' as Functions; // alias is Functions
 import 'package:DartLearning/functions/FunctionWithParameters.dart'; 
 import 'package:DartLearning/functions/FunctionWithNamedParameters.dart';
+import 'package:DartLearning/JsonSerilization/UserInfo.dart' as UserInfo;
+
 
 void main(List<String> arguments) {
   print('Hello world: ${DartLearning.calculate()}!');
   Functions.printMe();
   functionWithParameters("aromal"); // calling a function without aliases
   functionWithNamedParameter(name: "mahesh");
+   UserInfo.functionGetData().then((onValue){ 
+     print(onValue.info.seed);
+   });
 }
+
+
