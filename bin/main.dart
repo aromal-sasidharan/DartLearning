e8b26496ee2574735439217a86f37082fda910ec
Aromal <aromalsasidharan@gmail.com>
2020-03-29 18:16:03 +0800

    added sealed class example

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,11 @@
 import 'package:DartLearning/DartLearning.dart' as DartLearning; // alias is DartLearning
 import 'package:DartLearning/functions/function.dart' as Functions; // alias is Functions
-import 'package:DartLearning/functions/FunctionWithParameters.dart'; 
+import 'package:DartLearning/functions/FunctionWithParameters.dart';
+import 'package:DartLearning/sealed_class_ex/ApiRouter.dart';
+import 'package:DartLearning/sealed_class_ex/OnBoardingRouter.dart'; 
 
 void main(List<String> arguments) {
-  print('Hello world: ${DartLearning.calculate()}!');
-  Functions.printMe();
-  functionWithParameters("aromal"); // calling a function without aliases
+  var loginRequest = Login(username: "mahes",password: "xxxxx");
+  print("login body ${loginRequest.body}");
+
 }
